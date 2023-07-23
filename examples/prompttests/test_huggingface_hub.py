47b3a8dc95508123f17aabbefa0944acb735cef1
steven krawczyk <stevent.krawczyk@gmail.com>
2023-07-23 13:19:46 -0700

    Update HF example

--- examples/prompttests/test_huggingface_hub.py
+++ examples/prompttests/test_huggingface_hub.py
@@ -5,40 +5,29 @@
 # LICENSE file in the root directory of this source tree.
 
 import os
-from typing import Dict, Tuple
+import jinja2
 import prompttools.prompttest as prompttest
 from prompttools.utils import similarity
 from prompttools.experiment import HuggingFaceHubExperiment
-
-EXPECTED = {"Who was the first president of the USA?": "George Washington"}
 
 if not (("HUGGINGFACEHUB_API_TOKEN" in os.environ) or ("DEBUG" in os.environ)):
     print("Error: This example requires you to set either your HUGGINGFACEHUB_API_TOKEN or DEBUG=1")
     exit(1)
 
-
-def extract_responses(output) -> list[str]:
-    r"""
-    Helper function to unwrap the Hugging Face Hub repsonse object.
-    """
-    return [choice["generated_text"] for choice in output]
-
-
 @prompttest.prompttest(
     experiment=HuggingFaceHubExperiment,
     model_name="google/flan-t5-xxl",
     metric_name="similar_to_expected",
-    prompt_template="Question: {{input}}",
-    user_input=[{"input": "Who was the first president of the USA?"}],
+    eval_fn=similarity.evaluate,
+    expected="George Washington"
 )
-def measure_similarity(input_pair: Tuple[str, Dict[str, str]], results: Dict, metadata: Dict) -> float:
-    r"""
-    A simple test that checks semantic similarity between the user input
-    and the model's text responses.
-    """
-    expected = EXPECTED[input_pair[1]["input"]]
-    scores = [similarity.compute(expected, response) for response in extract_responses(results)]
-    return max(scores)
+def prompt_provider():
+    prompt_template="Answer the following question: {{ input }}"
+    user_input={"input": "Who was the first president of the USA?"}
+    environment = jinja2.Environment()
+    template = environment.from_string(prompt_template)
+    prompt = template.render(**user_input)
+    return [prompt]
 
 
 if __name__ == "__main__":
