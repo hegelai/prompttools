cef64ae1fcf3e24a525ec8c84f8220d7e547d838
steven krawczyk <stevent.krawczyk@gmail.com>
2023-07-24 20:25:59 -0700

    Update name

--- prompttools/selector/prompt_selector.py
+++ prompttools/selector/prompt_selector.py
@@ -4,7 +4,7 @@
 # This source code's license can be found in the
 # LICENSE file in the root directory of this source tree.
 
-TEMPLATE = """
+LLAMA_TEMPLATE = """
 <s>[INST] <<SYS>>
 {instruction}
 <</SYS>
@@ -30,4 +30,4 @@ class PromptSelector:
         ]
 
     def for_llama(self):
-        return TEMPLATE.format(instruction=self.instruction, user_input=self.user_input)
+        return LLAMA_TEMPLATE.format(instruction=self.instruction, user_input=self.user_input)
