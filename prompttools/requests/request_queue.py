daf52590d1170c8c2f718dd290ca9427f94fd5d9
steven krawczyk <stevent.krawczyk@gmail.com>
2023-07-26 10:35:29 -0700

    Cleanup

--- prompttools/requests/request_queue.py
+++ prompttools/requests/request_queue.py
@@ -39,14 +39,12 @@ class RequestQueue:
 
     def _do_task(self, fn: Callable, args: Dict[str, object]) -> None:
         try:
-            logging.info("Trying request")
             res = self._run(fn, args)
             self.request_results.append(res[0])
             self.request_latencies.append(res[1])
         # TODO: If we get an unexpected error here, the queue will hang
-        except Exception as e:
-            logging.error("Error: " + str(e))
-            # logging.error("Authentication error. Skipping request.")
+        except openai.error.AuthenticationError:
+            logging.error("Authentication error. Skipping request.")
 
     @retry_decorator
     def _run(self, fn: Callable, args: Dict[str, object]) -> Tuple[Dict[str, object], float]:
--- scripts/create_comment.py
+++ scripts/create_comment.py
@@ -3,8 +3,9 @@ from prompttools.selector.prompt_selector import PromptSelector
 
 PROMPTTOOLS_MD_TMP = "markdown.md"
 
-selectors = [PromptSelector("You are a helpful assistant.", "Is 17077 a prime number?")]
-models = ["gpt-3.5-turbo"]
+selectors = [PromptSelector("You are a helpful assistant.", "Is 17077 a prime number?"),
+             PromptSelector("You are a math tutor.", "Is 17077 a prime number?")]
+models = ["gpt-3.5-turbo", "gpt-4"]
 temperatures = [0.0]
 openai_experiment = OpenAIChatExperiment(models, selectors, temperature=temperatures)
 openai_experiment.run()
