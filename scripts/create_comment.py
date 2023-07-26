1c4ba74b7be9e53ba2ceee43dc3c192e480c6754
Steven Krawczyk (Hegel AI) <stevent.krawczyk@gmail.com>
2023-07-26 13:28:45 -0700

    Add action template (#32)
    * Add action template
    
    * Add permissions
    
    * Add variable
    
    * Add markdown fn and script
    
    * Fix gha
    
    * Formatting
    
    * Try require
    
    * Formatting
    
    * Try with secret
    
    * Debugging
    
    * Fix prompting
    
    * Cleanup
    
    * Fix markdown issue
    
    * Fix markdown
    
    * Switch back to DEBUG
    
    * Compress GIF

---
+++ scripts/create_comment.py
@@ -0,0 +1,15 @@
+from prompttools.experiment import OpenAIChatExperiment
+from prompttools.selector.prompt_selector import PromptSelector
+
+PROMPTTOOLS_MD_TMP = "markdown.md"
+
+selectors = [PromptSelector("You are a helpful assistant.", "Is 17077 a prime number?"),
+             PromptSelector("You are a math tutor.", "Is 17077 a prime number?")]
+models = ["gpt-3.5-turbo", "gpt-4"]
+temperatures = [0.0]
+openai_experiment = OpenAIChatExperiment(models, selectors, temperature=temperatures)
+openai_experiment.run()
+
+markdown = openai_experiment.to_markdown()
+with open(PROMPTTOOLS_MD_TMP, "w") as f:
+    f.write(markdown)
