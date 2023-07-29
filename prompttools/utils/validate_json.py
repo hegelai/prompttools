efbd29ab85768a375efbe85f74ff0ce49cd8cce1
Kevin <kevin@hegel-ai.com>
2023-07-28 17:38:21 -0700

    Add pre-processing function to validate JSON

--- prompttools/utils/validate_json.py
+++ prompttools/utils/validate_json.py
@@ -5,20 +5,53 @@
 # LICENSE file in the root directory of this source tree.
 
 
-from typing import Dict, List
+from typing import Callable, Dict, List, Optional
 import json
 import re
 
 KEY_EXTRACTION_REGEX = r'"([^"]+?)"\s*:'
 
 
-def validate(text: str):
+def strip_outer_brackets(text: str) -> str:
+    r"""
+    Removes all chars outside the first '{' and the last '}'. Intended to be a pre-processing
+    step prior to parsing a string as JSON.
+
+    Args:
+        text(str): the text to process
+    """
+    first_brace = text.find("{")
+    last_brace = text.rfind("}")
+    return text[first_brace: last_brace + 1]
+
+
+def sample_pre_process_fn(text: str):
+    r"""
+    An example pre-processing that you may use before attempting to parse a string as JSON.
+    This function removes all chars outside the first '{' and the last '}'. Then,
+    it removes ``"\\n"``.
+
+    This function should be modified depending on your LLM's output.
+
+    Args:
+        text(str): the text to process
+    """
+    text = strip_outer_brackets(text)
+    text = text.replace("\\n", "")
+    return text
+
+
+def validate(text: str, pre_process_fn: Optional[Callable] = None):
     r"""
     Validates that the generated text is JSON.
 
     Args:
         text (str): The generated text, which should be valid JSON.
+        pre_process_fn (Callable[str, str]): a function to pre-process the text response from the LLM before attempting
+            to parse the string as JSON. Look at ``validate_json.sample_pre_process_fn`` as an example.
     """
+    if pre_process_fn:
+        text = pre_process_fn(text)
     try:
         json.loads(text)
     except ValueError:
