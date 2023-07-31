d383e500d102ce957532e9c6ca8e4de28fcb3e6b
steven krawczyk <stevent.krawczyk@gmail.com>
2023-07-30 17:44:50 -0700

    Update theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,7 +35,7 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "groundwork"
 html_static_path = ["_static"]
 
 
