3e7ed8f7d9c993373a95a15eca1517df6ef03881
steven krawczyk <stevent.krawczyk@gmail.com>
2023-07-30 18:00:40 -0700

    Furo theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,7 +35,7 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "groundwork"
+html_theme = "furo"
 html_static_path = ["_static"]
 
 
