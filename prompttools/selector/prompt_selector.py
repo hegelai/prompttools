da0ce94f8883f3eb67573869b8dd85fd489d34f9
steven krawczyk <stevent.krawczyk@gmail.com>
2023-08-04 10:26:24 -0700

    Guard anthropic + version bump

--- prompttools/selector/prompt_selector.py
+++ prompttools/selector/prompt_selector.py
@@ -4,8 +4,12 @@
 # This source code's license can be found in the
 # LICENSE file in the root directory of this source tree.
 
+try:
+    from anthropic import HUMAN_PROMPT, AI_PROMPT
+except ImportError:
+    HUMAN_PROMPT, AI_PROMPT = None, None
 
-from anthropic import HUMAN_PROMPT, AI_PROMPT
+
 
 GENERIC_TEMPLATE = """INSTRUCTION:
 {instruction}
