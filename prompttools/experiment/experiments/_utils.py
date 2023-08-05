d7eb1a27faa275d7f9bea49c867c95198cb7402f
Kevin <kevin@hegel-ai.com>
2023-08-04 21:24:29 -0700

    Improve column uniqueness checking

--- prompttools/experiment/experiments/_utils.py
+++ prompttools/experiment/experiments/_utils.py
@@ -4,16 +4,29 @@
 # This source code's license can be found in the
 # LICENSE file in the root directory of this source tree.
 
-
 import pandas as pd
+
+
+def _check_column_uniqueness(column: "pd.core.series.Series") -> bool:
+    r"""
+    Check if all elements are equal in the column.
+
+    Arg:
+        column (pandas.core.series.Series): Column to check
+    """
+    first_ele = column[0]
+    for ele in column:
+        if first_ele != ele:
+            return True
+    return False
 
 
 def _get_dynamic_columns(df: pd.DataFrame) -> pd.DataFrame:
     r"""
-    Given a ``pd.DataFrame``
+    Given a ``pd.DataFrame``, return a DataFrame where columns have more than 1 unique value.
 
     Args:
-        df (pd.DataFrame):
+        df (pd.DataFrame): DataFrame to examine
     """
     hashable_columns = []
     unhashable_columns = []
@@ -22,7 +35,9 @@ def _get_dynamic_columns(df: pd.DataFrame) -> pd.DataFrame:
             hash(df[col][0])
             hashable_columns.append(col)
         except TypeError:
-            unhashable_columns.append(col)
+            # If a column is not hashable, check if there exists value differ from the
+            if _check_column_uniqueness(df[col]):
+                unhashable_columns.append(col)
 
     unique_counts = df[hashable_columns].nunique()
     columns_with_multiple_unique_values = unique_counts[unique_counts > 1].index
