a1b38b0d4f576cf50b324d084982006e2edf3489
steven krawczyk <stevent.krawczyk@gmail.com>
2023-08-10 09:28:41 -0700

    Small fixes

--- prompttools/playground/data_loader.py
+++ prompttools/playground/data_loader.py
@@ -58,15 +58,10 @@ def load_data(
     elif model_type == "Anthropic":
         experiment = EXPERIMENTS[model_type]([model], selectors, temperature=[temperature])
 
-    data_loader_response = {}
     try:
-        data_loader_response['df'] = experiment.to_pandas_df()
-        data_loader_response['to_mongo_db'] = experiment.to_mongo_db
+        return experiment.to_pandas_df(), experiment.to_mongo_db
     except Exception as e:
         raise Exception(str(e))
-
-
-    return data_loader_response
 
 
 @st.cache_data
--- prompttools/playground/playground.py
+++ prompttools/playground/playground.py
@@ -159,7 +159,7 @@ if mode == "Instruction":
     link += "&prompt=" + urllib.parse.quote(prompts[0])
 
     if run:
-        data_loader_response = load_data(
+        df, to_mongo_db = load_data(
             model_type,
             model,
             instructions,
@@ -171,8 +171,8 @@ if mode == "Instruction":
             presence_penalty,
             api_key,
         )
-        st.session_state.df = data_loader_response['df']
-        st.session_state.to_mongo_db = data_loader_response['to_mongo_db']
+        st.session_state.df = df
+        st.session_state.to_mongo_db = to_mongo_db
 
         for i in range(len(prompts)):
             for j in range(len(instructions)):
@@ -184,16 +184,16 @@ if mode == "Instruction":
             for j in range(len(instructions)):
                 placeholders[i][j + 1].markdown(df["response"][i + len(prompts) * j])
 
-    if("df" in st.session_state and not clear ):
+    if "df" in st.session_state and not clear:
         st.sidebar.info("Export To MongoDB")
         mongo_uri = st.sidebar.text_input("Enter MongoURI")
         database = st.sidebar.text_input("Enter Database Name")
         collection = st.sidebar.text_input("Enter Collection Name")
         export = st.sidebar.button("Export")
         loaded = False
-        if(export and mongo_uri and database and collection and export):
+        if export and mongo_uri and database and collection and export:
             with st.sidebar:
-                with st.spinner('Exporting...'):
+                with st.spinner("Exporting..."):
                     st.session_state.to_mongo_db(mongo_uri, database, collection)
                     st.write("Exported!")
 
@@ -268,14 +268,14 @@ elif mode == "Prompt Template":
 
     if run:
         prompts = render_prompts(templates, vars)
-        data_loader_response = load_data(model_type, model, [instruction], prompts, temperature, api_key)
+        df, to_mongo_db = load_data(model_type, model, [instruction], prompts, temperature, api_key)
         st.session_state.prompts = prompts
-        st.session_state.df = data_loader_response['df']
-        st.session_state.to_mongo_db = data_loader_response['to_mongo_db']
+        st.session_state.df = df
+        st.session_state.to_mongo_db = to_mongo_db
         for i in range(len(prompts)):
             for j in range(len(templates)):
                 placeholders[i][j + variable_count].markdown(st.session_state.df["response"][i + len(prompts) * j])
-    
+
     elif "df" in st.session_state and "prompts" in st.session_state and not clear:
         df = st.session_state.df
         prompts = st.session_state.prompts
@@ -283,13 +283,13 @@ elif mode == "Prompt Template":
             for j in range(len(templates)):
                 placeholders[i][j + variable_count].markdown(df["response"][i + len(prompts) * j])
 
-    if("df" in st.session_state and not clear ):
+    if "df" in st.session_state and not clear:
         st.sidebar.info("Export To MongoDB")
         mongo_uri = st.sidebar.text_input("Enter MongoURI")
         database = st.sidebar.text_input("Enter Database Name")
         collection = st.sidebar.text_input("Enter Collection Name")
-        export = st.sidebar.button("Export")        
-        if(export and mongo_uri and database and collection and export):
+        export = st.sidebar.button("Export")
+        if export and mongo_uri and database and collection and export:
             st.session_state.to_mongo_db(mongo_uri, database, collection)
 
 
