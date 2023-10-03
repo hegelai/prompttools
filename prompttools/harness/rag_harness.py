0206e8cc85e9f4bcc9fada5538601144828fd4f9
Kevin <kevin@hegel-ai.com>
2023-10-02 17:47:21 -0700

    Improving RAG harness to display relevant metadata in the final table

--- prompttools/harness/rag_harness.py
+++ prompttools/harness/rag_harness.py
@@ -42,7 +42,7 @@ def _generate_doc_prompt(documents: list[str], prompt_or_msg: Union[str, list[di
     if not is_chat:
         return doc_prompt
     else:
-        new_msg = copy.copy(prompt_or_msg)
+        new_msg = copy.deepcopy(prompt_or_msg)
         new_msg[-1]["content"] = doc_prompt
         return new_msg
 
@@ -59,6 +59,9 @@ class RetrievalAugmentedGenerationExperimentationHarness(ExperimentationHarness)
         llm_arguments (dict[str, list]): Dictionary of arguments for the LLM.
         extract_document_fn (Callable): A function, when given a row of results from the vector DB experiment,
             extract the relevant documents (``list[str]``) that will be inserted into the template.
+        extract_query_metadata_fn (Callable): A function, when given a row of results from the vector DB experiment,
+            extract the relevant metadata and return a ``str`` that will be shown for visualization in the final
+            result table
         prompt_template (str): A ``jinja``-styled templates, where documents and prompt will be inserted.
     """
 
@@ -68,6 +71,7 @@ class RetrievalAugmentedGenerationExperimentationHarness(ExperimentationHarness)
         llm_experiment_cls: Type[Experiment],
         llm_arguments: dict,
         extract_document_fn: Callable,
+        extract_query_metadata_fn: Callable,
         prompt_template: str = DOC_PROMPT_TEMPLATE,
     ):
         self.vector_db_experiment = vector_db_experiment
@@ -75,6 +79,7 @@ class RetrievalAugmentedGenerationExperimentationHarness(ExperimentationHarness)
         self.experiment: Optional[Experiment] = None
         self.llm_arguments = copy.copy(llm_arguments)
         self.extract_document_fn = extract_document_fn
+        self.extract_query_metadata_fn = extract_query_metadata_fn
         self.prompt_templates = prompt_template
 
     def run(self) -> None:
@@ -100,11 +105,19 @@ class RetrievalAugmentedGenerationExperimentationHarness(ExperimentationHarness)
 
         # Run the LLM experiment
         self.experiment.run()
-        # TODO: We can clean up results for visualization, ideally show these columns ("top doc ids", "prompt")
-        #       Right now it is displaying the fully augmented prompt for every row, which take up too much space
-        #       Note: Permutation ordering (doc_list1, prompt, then other arguments)
         self.partial_df = self.experiment.partial_df
         self.full_df = self.experiment.full_df
+
+        # Add "query text" (i.e. the prompt used to retrieve documents from the vector DB)
+        # to the final results table here
+        retrieval_n_rows = len(self.vector_db_experiment.full_df)
+        query_metadata = [
+            self.extract_query_metadata_fn(row) for _, row in self.vector_db_experiment.full_df.iterrows()
+        ]
+        final_n_row = len(self.full_df)
+
+        self.partial_df["retrieval_metadata"] = [query_metadata[i % retrieval_n_rows] for i in range(final_n_row)]
+        self.full_df["retrieval_metadata"] = self.partial_df["retrieval_metadata"]
 
     def visualize(self) -> None:
         if self.experiment is None:
