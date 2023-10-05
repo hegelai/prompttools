3cab7b32115a6ce20380ec8e8de42a4b851e74da
steven krawczyk <stevent.krawczyk@gmail.com>
2023-10-05 14:27:02 -0700

    Update to version 0.0.36

--- prompttools/version.py
+++ prompttools/version.py
@@ -1,2 +1,2 @@
-__version__ = '0.0.35a0+11a940e'
-git_version = '11a940ecfe36bf8dba8e384f08a9aecddb413d40'
+__version__ = '0.0.36a0+b87346b'
+git_version = 'b87346b7db47f062ff439ef096c19f23a4f60c31'
