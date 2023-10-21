7e373349b328b3d1308fc4e864fbc48676a28a6f
steven krawczyk <stevent.krawczyk@gmail.com>
2023-10-21 00:48:19 -0700

    Version bump, remove transformers req

--- prompttools/version.py
+++ prompttools/version.py
@@ -1,2 +1,2 @@
-__version__ = '0.0.36a0+b87346b'
-git_version = 'b87346b7db47f062ff439ef096c19f23a4f60c31'
+__version__ = '0.0.37a0+cc45f93'
+git_version = 'cc45f93f7133d87d85583232331e59ccf96d35ab'
