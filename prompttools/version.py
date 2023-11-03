1d2c9c60bb42a49e9bbc0e2917f102d83746d788
steven krawczyk <stevent.krawczyk@gmail.com>
2023-11-03 16:41:41 -0700

    Version bump

--- prompttools/version.py
+++ prompttools/version.py
@@ -1,2 +1,2 @@
-__version__ = '0.0.40a0+e60cb9e'
-git_version = 'e60cb9e454cdc78e1a790e1cd4e5e8d3f01773c3'
+__version__ = '0.0.41a0+6e6ef77'
+git_version = '6e6ef77d520abd969925039b5ababcd3c52fe679'
