f6a50e89056085c3a142aca62aa35338304c57aa
Kevin <kevin@hegel-ai.com>
2023-12-12 14:46:56 -0500

    Disable test_scribe as it is not meant for CI testing for now

--- test/test_scribe.py
+++ test/test_scribe.py
@@ -5,21 +5,22 @@
 # LICENSE file in the root directory of this source tree.
 
 
-import openai
-import os
-import prompttools.scribe  # noqa: F401 Importing this line will monkey-patch `openai.chat.completions.create`
+# import openai
+# import os
 
-if __name__ == "__main__":
+# import prompttools.scribe  # noqa: F401 Importing this line will monkey-patch `openai.chat.completions.create`
 
-    os.environ["OPENAI_API_KEY"] = ""
-
-    # Launch server from `app.py` first
-    # Example usage:
-    for i in range(3):
-        messages = [
-            {"role": "user", "content": f"What is 1 + {i}?"},
-        ]
-        result = openai.chat.completions.create(model="gpt-3.5-turbo", messages=messages, hegel_model="TEST_MODEL")
-        print(f"{i} {result = }")
-
-    print("End")
+# if __name__ == "__main__":
+#
+#     os.environ["OPENAI_API_KEY"] = ""
+#
+#     # Launch server from `app.py` first
+#     # Example usage:
+#     for i in range(3):
+#         messages = [
+#             {"role": "user", "content": f"What is 1 + {i}?"},
+#         ]
+#         result = openai.chat.completions.create(model="gpt-3.5-turbo", messages=messages, hegel_model="TEST_MODEL")
+#         print(f"{i} {result = }")
+#
+#     print("End")
