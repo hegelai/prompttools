6b2522d812f2fad7f81d09210b8f5ed29d3db2d3
Steven Krawczyk (Hegel AI) <stevent.krawczyk@gmail.com>
2023-12-29 11:02:18 -0800

    Add OpenAI client support to logger.py

--- prompttools/logger/logger.py
+++ prompttools/logger/logger.py
@@ -4,24 +4,22 @@
 # This source code's license can be found in the
 # LICENSE file in the root directory of this source tree.
 import json
-
 import requests
 import threading
 import queue
 from functools import partial
 import openai
+from openai import OpenAI
 import os
 from dotenv import load_dotenv
 from os.path import join, dirname
 from time import perf_counter
 from prompttools.common import HEGEL_BACKEND_URL
 
-
 # Load "OPENAI_API_KEY" into `os.environ["OPENAI_API_KEY"]`
 # See `.env.example`
 dotenv_path = join(dirname(__file__), ".env")
 load_dotenv(dotenv_path)
-
 
 class Logger:
     def __init__(self):
@@ -34,15 +32,16 @@ class Logger:
 
         self.worker_thread.start()
 
-    def execute_and_add_to_queue(self, callable_func, **kwargs):
+    def execute_and_add_to_queue(self, *args, **kwargs):
         if "hegel_model" in kwargs:
             hegel_model = kwargs["hegel_model"]
             del kwargs["hegel_model"]
         else:
             hegel_model = None
         start = perf_counter()
-        result = callable_func(**kwargs)
+        result = self.original_create(*args, **kwargs)
         latency = perf_counter() - start
+
         self.data_queue.put(
             {
                 "hegel_model": hegel_model,
@@ -53,8 +52,9 @@ class Logger:
         )
         return result
 
-    def wrap(self, callable_func):
-        return partial(self.execute_and_add_to_queue, callable_func)
+    def monkey_patch_method(self, method):
+        self.original_create = method
+        return self.execute_and_add_to_queue
 
     def worker(self):
         while True:
@@ -78,11 +78,15 @@ class Logger:
         except requests.exceptions.RequestException as e:
             print(f"Error sending data to Flask API: {e}")
 
+class CustomOpenAI(OpenAI):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.logger = Logger()
+        self.chat.completions.create = self.logger.monkey_patch_method(self.chat.completions.create)
 
-sender = Logger()
 # Monkey-patching
 try:
-    openai.chat.completions.create = sender.wrap(openai.chat.completions.create)
+    openai.OpenAI = CustomOpenAI
 except Exception:
     print("You may need to add `OPENAI_API_KEY=''` to your `.env` file.")
     raise
